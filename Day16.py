1e1d28fc0a3c437593da63b8522d2e1cba687899
John Teye Doku <johnteye2002@gmail.com>
2022-10-25 11:57:20 +0000

    PrettyTable

--- Day16.py
+++ Day16.py
@@ -1,8 +1,16 @@
-from turtle import Turtle, Screen
-jimmy = Turtle()
-jimmy.shape("arrow")
-print(jimmy)
+# from turtle import Turtle, Screen
+# jimmy = Turtle()
+# jimmy.shape("arrow")
+# print(jimmy)
 
-my_screen = Screen()
-print(my_screen.canvheight)
-my_screen.exitonclick()
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+from prettytable import PrettyTable
+table = PrettyTable()
+
+table.add_column("Pokemon Namee",["Pikachu", "Squirtle","Charmader"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+table.align = "l"
+
+print(table)
---
+++ Day17.py
@@ -0,0 +1,22 @@
+question_data = [
+{"text": "A slug's blood is green.", "answer": "True"},
+{"text": "The loudest animal is the African Elephant.", "answer": "False"},
+{"text": "Approximately one quarter of human bones are in the feet.", "answer": "True"},
+{"text": "The total surface area of a human lungs is the size of a football pitch.", "answer": "True"},
+{"text": "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", "answer": "True"},
+{"text": "In London, UK, if you happen to die in the House of Parliament, you are entitled to a state funeral.", "answer": "False"},
+{"text": "It is illegal to pee in the Ocean in Portugal.", "answer": "True"},
+{"text": "You can lead a cow down stairs but not up stairs.", "answer": "False"},
+{"text": "Google was originally called 'Backrub'.", "answer": "True"},
+{"text": "Buzz Aldrin's mother's maiden name was 'Moon'.", "answer": "True"},
+{"text": "No piece of square dry paper can be folded in half more than 7 times.", "answer": "False"},
+{"text": "A few ounces of chocolate can to kill a small dog.", "answer": "True"}
+]
+
+class Question:
+    def __init__(self, text, answer):
+        self.text = text
+        self.answer = answer
+
+new_q = Question("why are you coded", "that's just me")
+print(new_q.text, new_q.answer)
